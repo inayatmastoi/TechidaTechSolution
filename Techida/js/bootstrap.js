542e2c00dcdd3f8a20aed4858e1fa1ac66b77049
Inayat <141733608+inayatmastoi@users.noreply.github.com>
2024-07-29 10:15:14 +0500

    add techida

---
+++ Techida/js/bootstrap.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 200;
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 5);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+});
+
